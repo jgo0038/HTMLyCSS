44f5720fd0ac476deac204e7438ec188dbee66ec
Jorge Gomez <jgo0038@alu.ubu.es>
2021-05-13 12:58:16 +0200

    Fix jquery and imagemin

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
-    "minifyStyles": false ,
-    "mozjpeg": { "progressive": true, "quality": 10 },
+    "minifyStyles": false,
+    "mozjpeg": { "progressive": true, "quality": 50 },
     "pngquant": { "quality": [0.25, 0.5] },
     "svgo": {
         "plugins": [
